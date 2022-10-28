158e397fa2086b9810cf811998ff829189d56ce1
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-10-28 22:27:08 +0200

    Added iPad Pro
    Added Ipad pro section on the index page. Support for desktop and smaller devices.

--- index.php
+++ index.php
@@ -3,18 +3,36 @@
 <?php require_once 'header.php'; ?>
 </body>
 <main>
-    <div class="indexIpad">
+    <div class="indexIPad">
         <div class="wrapper">
-            <img src="img/ipad1.jpg" alt="">
-            <div class="indexIpadText">
+            <img src="img/iPad1.jpg" alt="">
+            <div class="indexIPadText">
                 <h2>iPad</h2>
                 <p>Lovable. Drawable. Magical.</p>
-                <div class="indexIpadText2">
-                    <a href=""><span id="IndexIpadText2A">Learn more</span> ></a>
-                    <a href=""><span id="IndexIpadText2A">Buy</span> ></a>
+                <div class="indexIPadText2">
+                    <a href=""><span id="indexIPadText2A">Learn more</span> ></a>
+                    <a href=""><span id="indexIPadText2A">Buy</span> ></a>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="indexIPadPro">
+        <div class="wrapper">
+            <img src="img/iPadPro1.jpg" alt="">
+            <div class="indexIPadProText">
+                <h2>iPad Pro</h2>
+                <div class="indexIPadProText2">
+                    <p>Supercharged by</p>
+                    <img src="img/m2Logo.png" alt="">
+                </div>
+                <div class="indexIPadProText3">
+                    <a href=""><span id="indexIPadProText3A">Learn more</span> ></a>
+                    <a href=""><span id="indexIPadProText3A">Buy</span> ></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="whiteLine">
     </div>
 </main>
 </html>
